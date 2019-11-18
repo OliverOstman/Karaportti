0c55402e3d0758acaba173aab69b34ea5ae2f3c1
OliverOstman <oliver.ostman@metropolia.fi>
2019-11-18 15:10:58 +0200

    Update

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,19 +1,65 @@
 import React, {Component} from "react";
 
 class Settings extends Component{
+    red = "";
+    Text = "";
+    select = "";
+    ans = "";
+    remove = "";
+    text2 = "";
+
+    componentDidMount() {
+        this.red = document.getElementById("red");
+        this.Text = document.getElementById("redText");
+        this.select = document.getElementById("select");
+        this.ans = document.getElementById("ans");
+        this.remove = document.getElementById("remove");
+        this.text2 = document.getElementById("palo");
+    }
+
+    getText = (e) => {
+        e.preventDefault();
+        this.Text.style.display = "";
+        this.Text.innerText = this.red.value;
+        this.remove.style.display = "";
+    };
+
+    removeText = (e) => {
+        e.preventDefault();
+        this.Text.style.display = "none";
+        this.Text.innerText = "";
+        this.remove.style.display = "none";
+    };
+
+    setText = (e) => {
+        e.preventDefault();
+        /*
+        console.log(modal.innerText);
+        console.log(text2.value);
+        modal.innerText = text2.value;
+        */
+    };
+
+    getValue = (e) => {
+        e.preventDefault();
+        console.log(this.select.options[this.select.selectedIndex].value);
+        console.log(this.select.options[this.select.selectedIndex].text);
+        this.ans.innerText = '"' + this.select.options[this.select.selectedIndex].text + '"' + " Selected";
+    };
+
     render() {
         return(
             <div>
-                <h1>Tässä tärkeä tiedote</h1>
+                <h1 id="redText">Tässä tärkeä tiedote</h1>
                 <div>
                     <h1>Taustakuva</h1>
-                    <select>
+                    <select id="select">
                         <option value="1">Talvi</option>
                         <option value="2">Kevät</option>
                         <option value="3">Kesä</option>
                         <option value="4">Syksy</option>
                     </select>
-                    <button onClick="">Select</button>
+                    <button onClick={this.getValue}>Select</button>
                     <p id="ans"></p>
                     <a href="https://webdev-public.metropolia.fi/taustakuvat/">Lista jtn oma.metropolian käyttämiä taustakuvia</a>
                     <p>(Lisänä pyhät/lomat joulu, pääsiäinen, halloween?)</p>
@@ -24,8 +70,8 @@ class Settings extends Component{
                     <textarea id="red">
                         Tähä laittaa jtn joka näkyy sitte sillain ylälaidassa punaisella taustalla
                     </textarea>
-                    <button onClick="">Send</button>
-                    <button id="remove" onClick="">Remove</button>
+                    <button onClick={this.getText}>Send</button>
+                    <button id="remove" onClick={this.removeText}>Remove</button>
                 </div>
                 <div>
                     <h1>Palohälytys</h1>
