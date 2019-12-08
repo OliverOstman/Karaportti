9e9ae34f0aca72183f6885258a63911105311142
OliverOstman <oliver.ostman@metropolia.fi>
2019-12-08 21:09:01 +0200

    update

--- src/components/Hsl.js
+++ src/components/Hsl.js
@@ -8,6 +8,7 @@ class Hsl extends Component{
     iframe;
     interval;
 
+    // If need/want make so that you can change the intervals time on settings
     componentDidMount() {
         this.nav = document.getElementById("Navbar");
         this.iframe = document.getElementById("iframe");
@@ -17,12 +18,14 @@ class Hsl extends Component{
                 console.log("vaihto");
                 if (this.state.iframeSrc === 1) {
                     this.setState({iframeSrc: 2});
+                } else if (this.state.iframeSrc === 2) {
+                    this.setState({iframeSrc: 3});
                 } else {
                     this.setState({iframeSrc: 1});
                 }
             }
                 .bind(this),
-            30000
+            15000
         );
     }
 
@@ -32,11 +35,16 @@ class Hsl extends Component{
                 <iframe id="iframe" title="HSL" src="https://hsl.trapeze.fi/traveller/web?command=fullscreen&id=MetAKK&title=Karaportti%202&cols=2&offset=180"
                 key={1}/>
             );
-        } else {
+        } else if (this.state.iframeSrc === 2) {
             return (
                 <iframe id="iframe" title="HSL" src="http://users.metropolia.fi/~villeope/ruokalista/"
                 key={2}/>
             );
+        } else {
+            return (
+                <iframe id="iframe" title="HSL" src="http://users.metropolia.fi/~tuomasmy/infonaytto/info.html"
+                        key={3}/>
+            )
         }
     };
 
